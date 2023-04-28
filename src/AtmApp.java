20f33cad469d3c2f43ad52f0f0b2ce5799f1dc72
Rana Betul Turkmen <ranabetulturkmen@gmail.com>
2023-04-28 14:57:49 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,8 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("bu bir atm projesi");
+        //version 1
+        System.out.println("java kolay");
+        //version 2
     }
 }
